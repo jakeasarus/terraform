db96c9821a983d7abc49ae7c74f20ef933f964e6
jakeasarus <jjones@thinkstack.co>
2020-10-21 05:02:17 -0400

    added lifecycle to ignore ami changes

--- ec2_server_2019_use1/main.tf
+++ ec2_server_2019_use1/main.tf
@@ -3,6 +3,10 @@ resource "aws_instance" "example" {
     ami = data.aws_ami.windows.id
     instance_type = "t2.micro"
     availability_zone = var.availability_zone
+  
+  lifecycle {
+    ignore_changes = ["ami"]
+  }
 }
 
 #AMI Filter for Windows Server 2019 Base
