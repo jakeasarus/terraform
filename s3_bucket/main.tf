7d643bc9f4e6c43669bdadce0a9c58363b8bfb96
rajvasupilli <30533295+rajvasupilli@users.noreply.github.com>
2021-05-28 18:27:09 +0530

    Update main.tf

--- s3_bucket/main.tf
+++ s3_bucket/main.tf
@@ -7,18 +7,18 @@ resource "aws_s3_bucket" "b" {
     }
 
     logging {
-        target_bucket = var.target_bucket
-        target_prefix = var.target_prefix
+        target_bucket   = var.target_bucket
+        target_prefix     = var.target_prefix
     }
 
     server_side_encryption_configuration {
-    rule {
+    rule   {
       apply_server_side_encryption_by_default {
-        kms_master_key_id = var.kms_master_key_id
-        sse_algorithm     = var.sse_algorithm
+        kms_master_key_id   = var.kms_master_key_id
+        sse_algorithm  = var.sse_algorithm
       }
     }
   }
 
-    tags = var.tags
+    tags                      = var.tags
 }
