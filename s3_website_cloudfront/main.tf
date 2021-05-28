0b113f2c6d0ba28a45f257d9c98bb2ff1a1a02fa
rajvasupilli <30533295+rajvasupilli@users.noreply.github.com>
2021-05-28 18:27:46 +0530

    Update main.tf

--- s3_website_cloudfront/main.tf
+++ s3_website_cloudfront/main.tf
@@ -4,19 +4,19 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "prod_website" {
-  bucket_prefix = var.bucket_prefix
+  bucket_prefix     = var.bucket_prefix
   acl    = "public-read"
 
   website {
-    index_document = "index.html"
-    error_document = "error.html"
+    index_document    = "index.html"
+    error_document      = "error.html"
 
   }
 }
 
 
 resource "aws_s3_bucket_policy" "prod_website" {
-  bucket = aws_s3_bucket.prod_website.id
+  bucket    = aws_s3_bucket.prod_website.id
 
   policy = <<POLICY
 {
